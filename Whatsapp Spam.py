537ccafad23a534fb2b1bf698c9623e89bc31e97
Kaushik-Dhoot <88180795+Kaushik-Dhoot@users.noreply.github.com>
2022-06-07 23:59:00 +0530

    Delete Whatsapp Spam.py

--- Whatsapp Spam.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import webbrowser as wb
-import time
-
-wb.open("https://web.whatsapp.com/")
-time.sleep(10)
-for i in range(5):
-    pyautogui.write("Full Practice")
-    pyautogui.press("enter")
